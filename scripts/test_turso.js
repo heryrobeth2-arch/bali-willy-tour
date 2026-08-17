e220682a36dabbed7a4ec56fae3f6d42704b88a4
robeth hery purnomo <heryrobeth2@gmail.com>
2026-08-17 07:21:37 +0000

    44378647-b273-42d7-9f8a-da6fadd3c3d2

--- scripts/test_turso.js
+++ scripts/test_turso.js
@@ -1,6 +1,7 @@
 const { createClient } = require("@libsql/client");
 const turso = createClient({
-  url: "libsql://bali-willy-tour-purnomo.aws-ap-northeast-1.turso.io?authToken=eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3RjRFT0pvREVmR25xR1l5ZXRWX0tnIiwib3JnX2lkIjoxMDAwMjIzMDMwfQ.YWtDRB2JeorruW0EnKwIT8oTHrqzlnOng4bR132msO9nuRIzkhn3N26NCfbh4pDdLKikOka8mgsLTZTxIh91CA"
+  url: "libsql://bali-willy-tour-purnomo.aws-ap-northeast-1.turso.io",
+  authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3ODY5NDk1NDYsImlkIjoiMDFhMDBlNGEtODAwMS03Mjk3LTlkNzEtOTllY2I0Mjc5NDIwIiwia2lkIjoiSE9sX3VBUUhHV1o0NEJMQ2FsdUZqeEY4MGpPX2NzR004U2llUVpMTTdiQSIsInJpZCI6ImM0NDBjNzc4LTJlMDAtNDU2ZC04NmYyLWJiMGNkODhmYTU0NiJ9.iz6GZtr_i1_evr4_OW4WrrR8pMCE4xNTdS3z6LK_cLmVkX9HCPkAdJ2YtFzeoZTZgZVM7EXmdgmZ0h5xR0KHDA"
 });
 turso.execute("SELECT 1 as test").then(r => {
   console.log("SUCCESS:", r.rows);
